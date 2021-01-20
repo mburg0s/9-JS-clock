ed588054f0ad3ee3b289b1368c8ca4d77f645ae2
mburg0s <burgos.marissa@gmail.com>
2021-01-19 22:40:53 -0500

    updated js

--- main.js
+++ main.js
@@ -1,21 +1,34 @@
 
-  
 let index = 0
  let hexColor=''
 const currentTime = setInterval(time, 1000)
 
+// function to add Zero for single digit
+ function addZero(num){ 
+     if (String(num).length == 1) {
+         console.log(num)
+         console.log(String(num).length)
+         return '0' + num
+
+     } 
+     return num
+  }
 function time() {
 
 // for the current time display 
     const curDate = new Date()
-    let curHour = curDate.getHours()
-    if (curHour===0) {
-        curHour = 12
-    } else if (curHour>12) {
-        curHour = '0'+ curHour - 12
-    } curHour = '0'+ curHour
-    const curMinute = curDate.getMinutes()
-    const curSecond = curDate.getSeconds()
+
+// convert to standard format time
+    let cHour = curDate.getHours()
+    if (cHour===0) {
+        cHour = 12
+    } else if (cHour>12) {
+        cHour = cHour - 12
+    } 
+
+    const curHour = addZero(cHour)
+    const curMinute = addZero(curDate.getMinutes())
+    const curSecond = addZero(curDate.getSeconds())
     const cTime = `${curHour}:${curMinute}:${curSecond}`
     document.querySelector('#curTime').innerHTML = cTime
 
